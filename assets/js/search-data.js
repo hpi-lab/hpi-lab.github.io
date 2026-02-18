cbdf78badd0a790caa49e6dbda948ec9d40d73bd
hpi-lab <hpi.lab.official@gmail.com>
2026-02-18 16:29:18 +0000

    Deploying to gh-pages from @ hpi-lab/hpi-lab.github.io@d14a40c61e92e2aca8fed89f20b99af2f0dc48a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-paper-about-modeling-cumulative-arm-fatigue-in-mid-air-interaction-is-accepted-to-international-journal-of-human-computer-studies-ijhcs",
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "&#39;&#39;Instruct the wise, and they will be even wiser. Teach the righteous, and they will learn even more.&#39;&#39; - Proverbs 9:9",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "news-a-paper-about-modeling-cumulative-arm-fatigue-in-mid-air-interaction-is-accepted-to-international-journal-of-human-computer-studies-ijhcs",
           title: 'A paper about “Modeling Cumulative Arm Fatigue in Mid-Air Interaction” is accepted to...',
           description: "",
           section: "News",},{id: "news-a-paper-about-structural-and-temporal-cross-modal-distillation-is-accepted-to-neurips-2023",
@@ -52,62 +54,14 @@ ninja.data = [{
           section: "News",},{id: "news-a-paper-about-a-dynamic-action-model-based-vision-language-action-framework-for-robot-manipulation-is-accepted-to-icra-2026",
           title: 'A paper about “A Dynamic Action Model-Based Vision-Language-Action Framework for Robot Manipulation” is...',
           description: "",
-          section: "News",},{id: "news-human-centered-physical-intelligence-hpi-lab-is-officially-opened-at-the-department-of-artificial-intelligence-hanyang-university-erica",
-          title: 'Human-centered Physical Intelligence (HPI) Lab is officially opened at the Department of Artificial...',
-          description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project.html";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project.html";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
+            },},{id: "teachings-deep-learning",
+          title: 'Deep Learning',
+          description: "",
           section: "Teachings",handler: () => {
-              window.location.href = "/teachings/data-science-fundamentals.html";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning.html";
+              window.location.href = "/teachings/deep-learning-cse4048.html";
             },},];
